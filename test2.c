59baf96b48dc6d93c3a06169cbd5e8eebb487635
baekingwon <inwns@naver.com>
2016-06-29 13:48:37 +0900

    first comit

---
+++ test2.c
@@ -0,0 +1,126 @@
+//test2 2016.6.29 수 day2
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "engine2d.h"
+
+
+
+int main(){
+/*
+	char *code = "★"; //문자열 포인터 선언
+	printf("☏ \r\n"); //유니코드
+	printf("%s \r\n",code); 
+    printf("%x,%x,%x \r\n",code[0],code[1],code[2]); //코드로변환
+	
+	//e296
+
+	printf("\u96e2\r\n"); 
+
+
+
+int clear_o(){
+	Sleep(2000);
+    system("clear");
+	Sleep(2000);
+}
+
+/////////////////////////////////////////////// part1
+	clear_o();
+///////////////////////////////////////////////
+
+
+	
+      int num1 = 10 , num2 = 20;
+      printf("%d %d\r\n",num1, num2);
+
+      int num3, num4;
+	  num3 = 30;
+	  num4 = 40;
+	  printf("%d %d\r\n",num3, num4);
+
+
+/////////////////////////////////////////////// part2
+	clear_o();
+///////////////////////////////////////////////
+
+        int num5, num6;
+        printf("1값을 입력하세요: ");
+		scanf("%d,%d", &num5,&num6);
+		printf("%d+%d=%d\r\n",num5, num6, num5+num6);
+        printf("%d-%d=%d\r\n",num5, num6, num5-num6);
+        printf("%d×%d=%d\r\n",num5, num6, num5*num6);
+	    printf("%d÷%d=%d\r\n",num5, num6, num5/num6);
+
+/////////////////////////////////////////////// part3
+	clear_o();
+///////////////////////////////////////////////
+
+
+	   float num7, num8;
+	   //num7 = 3.14;
+	   //num8 = 9.8;
+        printf("2값을 입력하세요: ");
+	    scanf("%f,%f",&num7,&num8);
+	    printf("%f+%f=%f\r\n",num7, num8, num7+num8);
+        printf("%f-%f=%f\r\n",num7, num8, num7-num8);
+        printf("%f×%f=%f\r\n",num7, num8, num7*num8);
+	    printf("%f÷%f=%f\r\n",num7, num8, num7/num8);
+
+
+/////////////////////////////////////////////// part4
+	clear_o();
+///////////////////////////////////////////////
+
+
+
+        char *name = "Andrew Thomas Weil";
+	    printf("%s \r\n",name);
+
+		
+		char first_name[32];
+		char last_name[32];
+		printf("이름을 입력하세요: ");
+		scanf("%s %s",first_name,last_name);
+		printf("my name is %s %s \r\n",first_name,last_name);
+
+
+/////////////////////////////////////////////// part5
+	clear_o();
+/////////////////////////////////////////////// 문자열
+	
+
+       //str은 string
+	  //char *strTemp = "hello world!";
+		char strTemp[] = "hello world!";
+		int index;
+
+		
+		printf("%s \r\n",strTemp);
+		printf("%c \r\n",101);  //아스키코드
+		printf("문자를 선택하세요...\r\n");
+
+		scanf("%d",&index);
+
+		strTemp[index] = strTemp[index] + 1; //암호화
+
+		printf("%c , %d \r\n",strTemp[index],strTemp[index]);
+
+
+
+/////////////////////////////////////////////// part6
+	clear_o();
+/////////////////////////////////////////////// 2d엔진헤더연결테스트
+	
+*/
+	
+	setColor(32,44);
+	printf("hello");
+	setColor(0,0);
+
+	
+
+	return 0;
+}
